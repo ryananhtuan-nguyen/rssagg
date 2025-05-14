964c96c2b078fa4391fc0ce396b84cfa8afa4e8b
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2025-05-14 22:44:20 +1200

    feat: error handling

--- json.go
+++ json.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+func responseWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5xx error:", msg)
+	}
+	type ErrorResponse struct {
+		Error string `json:"error"`
+	}
+	responseWithJSON(w, code, ErrorResponse{Error: msg})
+}
+
 func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
